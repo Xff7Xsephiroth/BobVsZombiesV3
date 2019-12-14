23dd69ae0897d657aade970db6fab3d076ca99d7
Xff7Xsephiroth <55700964+Xff7Xsephiroth@users.noreply.github.com>
2019-12-14 14:02:24 +0000

    imported package

---
+++ BoB Vs Zombies(and other stuff) V2/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+public class Player : MonoBehaviour
+{
+    public delegate void UpdateHealth(int newHealth);
+    public static event UpdateHealth OnUpdateHealth;
+
+    private Animator gunAnim;
+    private void Start()
+    {
+        gunAnim = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            gunAnim.SetBool("IsFiring", true);
+        }
+        else
+        {
+            gunAnim.SetBool("IsFiring", false);
+        }
+    }
+    public void SendHealthData(int health)
+    {
+        if (OnUpdateHealth != null)
+        {
+            OnUpdateHealth(health);
+        }
+    }
+
+}
