03014b84723d4acf85e07dafa061ed97d81f617f
Xff7Xsephiroth <55700964+Xff7Xsephiroth@users.noreply.github.com>
2019-12-29 11:58:19 +0000

    started to make menu

---
+++ BoB Vs Zombies(and other stuff) V2/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+    
+{
+    public Button newGameButton;
+
+    public Button highScoreButton;
+
+    public Button exitGameButton;
+
+    public string newGameSceneName;
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(Zombie,Shooter,Level,1);
+
+    }
+
+    public void HighScore()
+    {
+
+
+    }
+        
+        
+    public void ExitGame()
+    {
+
+        Application.Quit();
+    }
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
