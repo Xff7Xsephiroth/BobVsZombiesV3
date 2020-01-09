d637f036fe60a091f0df5e8ada2ca78afd35bd16
Xff7Xsephiroth <55700964+Xff7Xsephiroth@users.noreply.github.com>
2020-01-09 11:59:45 +0000

    added gameover script and music

---
+++ BoB Vs Zombies(and other stuff) V2/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public void Music()
+    
+    {
+
+        AudioSource audioSource = GetComponent<AudioSource>();
+        audioSource.PlayDelayed(10);
+    }
+
+}
---
+++ BoB Vs Zombies(and other stuff) V2/Assets/Scripts/MusicTimer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicTimer : MonoBehaviour
+{
+   
+   
+    public float time = 3f; //30 seconds for you
+
+    public void Update()
+    {
+        if (time > 0)
+        {
+            time -= Time.deltaTime;
+        }
+        else
+        {
+            
+            GetComponent<AudioSource>().Play();
+        }
+    }
+
+        }
