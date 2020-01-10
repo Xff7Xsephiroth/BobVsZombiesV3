dafd4695e4fe7ba3e13a0b5317dd535024e18c66
Xff7Xsephiroth <55700964+Xff7Xsephiroth@users.noreply.github.com>
2020-01-10 15:10:17 +0000

    tried to fix score and enemy health issue

---
+++ BoB Vs Zombies(and other stuff) V2/Assets/Scripts/GameOverButtons.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverButtons : MonoBehaviour
+{
+    public void gameStart()
+    {
+
+        SceneManager.LoadScene(1);
+
+    }
+
+    public void gameExit()
+    {
+        Application.Quit();
+
+
+    }
+
+
+
+
+
+
+}
